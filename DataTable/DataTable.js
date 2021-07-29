daf81423f3b49670148c27881453804aff210e77
Muhammad Rafeh <muhammadrafehatique@gmail.com>
2021-07-29 18:49:44 +0500

    Changing the layout

--- DataTable/DataTable.js
+++ DataTable/DataTable.js
@@ -1,6 +1,12 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Dimensions } from 'react-native';
 import DataTableRow from './DataTableRow';
+
+const {width, height} = Dimensions.get('window');
+
+const MARGIN_HORIZONTAL = 20;
+const MARGIN_TOP = 20;
+const MARGIN_BOTTOM = 5;
 
 const DataTable = props => {
     // props will be array of objects
@@ -22,21 +28,23 @@ const DataTable = props => {
 
     return (
         <View style={styles.componentContainer}>
+
             <View style={styles.headerContainer}>
+
                 <View style={styles.firstColContainer}>
                     <Text style={styles.firstColLabel}>Name</Text>
                 </View>
-                <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-around', marginRight: 20 }}>
-                    <View style={{ flex: 1 }}>
-                        <Text style={{ color: 'grey', fontSize: 12, textAlign: 'right' }}>Price</Text>
-                    </View>
-                    <View style={{ flex: 1 }}>
-                        <Text style={{ color: 'grey', fontSize: 12, textAlign: 'right', paddingLeft: 15, paddingRight: 4 }}>Select</Text>
-                    </View>
+                {/* <View style={{ flexDirection: 'row', justifyContent: 'space-around', marginRight: 20 }}> */}
+                <View style={{width: '20%'}}>
+                    <Text style={{ color: 'grey', fontSize: 12, textAlign: 'right' }}>Price</Text>
                 </View>
+                <View style={{width: '30%'}}>
+                    <Text style={{ color: 'grey', fontSize: 12, textAlign: 'right', paddingLeft: 15, paddingRight: 4 }}>Select</Text>
+                </View>
+                {/* </View> */}
             </View>
 
-            <View style={{ height: 0.2, backgroundColor: 'grey', width: '100%', marginTop: 18, marginBottom: 10 }} />
+            <View style={styles.line} />
 
             {props.data && props.data.map((item, index) => <DataTableRow key={index} data={item} getRowSelectedData={getRowSelectedData} />)}
         </View>
@@ -47,18 +55,32 @@ export default DataTable;
 
 const styles = StyleSheet.create({
     componentContainer: {
-        marginTop: 20,
-        marginBottom: 5
+        marginHorizontal: MARGIN_HORIZONTAL,
+        marginTop: MARGIN_TOP,
+        marginBottom: MARGIN_BOTTOM
     },
     headerContainer: {
+        // flex: 1, 
         flexDirection: 'row',
-        justifyContent: 'space-between'
+        // justifyContent: 'space-between'
+        // marginHorizontal: MARGIN_HORIZONTAL,
     },
     firstColContainer: {
-        flex: 1, marginLeft: 20
+        // flex: 1, 
+        // marginLeft: 20
+        width: '50%',
+        // backgroundColor: 'green'
     },
     firstColLabel: {
-        color: 'grey', 
+        color: 'grey',
         fontSize: 12
+    },
+    line: {
+        height: 0.2, 
+        backgroundColor: 'grey', 
+        width,
+        marginTop: 18, 
+        marginBottom: 10,
+        alignSelf: 'center'
     }
 });
