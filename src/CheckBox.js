63a0c2786888ad92ddb218b78b92ae49d19d2beb
Muhammad Rafeh <muhammadrafehatique@gmail.com>
2022-03-25 17:06:17 +0500

    Updated Selection!

--- src/CheckBox.js
+++ src/CheckBox.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import { View, Image, StyleSheet, TouchableOpacity } from 'react-native';
-import PropTypes from 'prop-types';
 
 const CheckBox = React.memo((props) => { //props: initialVal
 
-  const {initialVal, handleOnRowSelect, info} = props;
-  
+  const { initialVal, handleOnRowSelect, info } = props;
+
   return (
-      <TouchableOpacity style={styles.touchableOpacity} onPress={handleOnRowSelect?.bind(null, !initialVal, info.id, info.name)} disabled={props?.disabled}>
-        <View style={[styles.backLayer, {backgroundColor: initialVal ? 'rgba(42, 187, 155, 0.1)' : 'transparent'}]} />
-        <View style={[styles.container, { backgroundColor: initialVal ? (props?.backgroundColor ? props?.backgroundColor : 'blue') : 'transparent', borderColor: props?.backgroundColor ? props?.backgroundColor : 'blue'  }]}>
-          { initialVal && ( <Image source={require('../assets/tick.png')} style={[{tintColor: props?.tickColor ? props.tickColor: 'white'} ]} resizeMode={'cover'} /> )}
-        </View>
-      </TouchableOpacity>
+    <TouchableOpacity style={styles.touchableOpacity} onPress={handleOnRowSelect?.bind(null, !initialVal, info.id, info.name)} disabled={props?.disabled}>
+      <View style={[styles.container, { backgroundColor: initialVal ? (props?.backgroundColor ? props?.backgroundColor : 'blue') : 'transparent', borderColor: props?.backgroundColor ? props?.backgroundColor : 'blue' }]}>
+        {initialVal && (<Image source={require('../assets/tick.png')} style={[{ tintColor: props?.tickColor ? props.tickColor : 'white', width: 15, height: 15 }]} resizeMode={'cover'} />)}
+      </View>
+    </TouchableOpacity>
   );
 })
 
@@ -29,25 +27,9 @@ const styles = StyleSheet.create({
     borderRadius: 2,
   },
   touchableOpacity: {
-    width: 35, 
-    height: 33, 
-    justifyContent: 'center', 
+    width: 35,
+    height: 33,
+    justifyContent: 'center',
     alignItems: 'center'
-    // borderWidth: 1
-  },
-  backLayer: {
-    position: 'absolute', 
-    top: 4.5, 
-    left: 6, 
-    width: 24, 
-    height: 24, 
-    borderRadius: 3,
   }
 })
-
-CheckBox.propTypes = {
-  onPress: PropTypes.func,
-  disabled: PropTypes.bool,
-  tickColor: PropTypes.string,
-  backgroundColor: PropTypes.string
-}
